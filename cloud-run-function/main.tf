e6fd14937251cbbd6527c35c925965f7fb2ef0cc
bcaliva21 <bcaliva21@gmail.com>
2024-11-23 00:36:14 -0600

    init commit

---
+++ cloud-run-function/main.tf
@@ -0,0 +1,31 @@
+resource "google_storage_bucket" "bucket" {
+  name     = "cloud-run-function-bucket"
+  location = "US"
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name   = "index.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "./hello/index.zip"
+}
+
+resource "google_cloudfunctions_function" "function" {
+  name        = "hello-function"
+  description = "Say hello"
+  runtime     = "nodejs16"
+
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = true
+  entry_point           = "index.js"
+}
+
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
